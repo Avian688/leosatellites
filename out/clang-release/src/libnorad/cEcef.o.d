e3bd90c6cda93c4d368cb3c8de2fef271991b1a3
Aiden Valentine <70658054+Avian688@users.noreply.github.com>
2021-07-26 14:55:42 +0100

    Delete out directory

--- out/clang-release/src/libnorad/cEcef.o.d
+++
@@ -1,9 +0,0 @@
-../out/clang-release/src/libnorad/cEcef.o: libnorad/cEcef.cc \
-  libnorad/cEcef.h ../../os3/src/libnorad/ccoord.h \
-  ../../os3/src/libnorad/globals.h
-
-libnorad/cEcef.h:
-
-../../os3/src/libnorad/ccoord.h:
-
-../../os3/src/libnorad/globals.h:
