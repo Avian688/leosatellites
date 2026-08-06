66b2eff86da4ffd59640b42a8dde170a66e43d6d
Gastón Frenkel <gst.frenkel@gmail.com>
2026-08-06 00:44:09 -0300

    Simplify phaseOffsetVal formula

--- src/libnorad/cOrbitA.cc
+++ src/libnorad/cOrbitA.cc
@@ -36,12 +36,14 @@ cOrbitA::cOrbitA(std::string satNameA, int epochY, double epochD, double altitud
    bstar = bstarA;
    drag = dragA;
    //int offsetVal = planes; //offsetVal must be changed according to constellation, used to prevent collisions
+   const int satelliteCount = satPerPlane * planes;
    const int currentPlane = satIndex / satPerPlane;
    const int planeIndex = satIndex % satPerPlane; //index of a satellite within a plane
 
+   const double phaseOffsetVal = static_cast<double>(currentPlane * phaseOffset * 360) / static_cast<double>(satelliteCount);
+
    raan = 360.0 / planes * currentPlane * RADS_PER_DEG; //RAAN value, uniformly created so that there are equally spaced orbital planes for even coverage.
 
-   const double phaseOffsetVal = 360.0 / satPerPlane * (static_cast<double>(phaseOffset) / static_cast<double>(planes)) * currentPlane;
    meanAnomaly = (360.0 / satPerPlane * planeIndex + phaseOffsetVal) * RADS_PER_DEG;
 
    m_jdEpoch = cJulian(epochYear, epochDay);
